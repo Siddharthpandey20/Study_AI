2aac93320638154eb5ee46f8cda3f12c017d370f
Siddharthpandey20 <siddharthpandey2611@gmail.com>
2025-06-21 17:59:08 +0530

    Hiding API Key

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -9,9 +9,9 @@ import time
 
 # Configure Cloudinary
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME", "dc1hfae8s"),
-    api_key=os.getenv("CLOUDINARY_API_KEY", "323918263282959"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET", "O1h5tqxhWvEo-dMxS1Ibp-g6kHA")
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 
 async def upload_file_to_cloudinary(file_content: bytes, filename: str, user_id: int):
